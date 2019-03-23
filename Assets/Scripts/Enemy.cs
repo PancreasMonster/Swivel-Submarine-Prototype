daedcf637e4cc35f83ebbca2d783350b517abe82
PancreasMonster <ryanflood444@gmail.com>
2019-03-23 12:59:37 +0000

    Added Enemy Spawning (Press Space for Now)

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject player;
+    public float timeOfArrival;
+    float distance;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distance = Vector3.Distance(transform.position, player.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = player.transform.position - transform.position;
+        dir.Normalize();
+        transform.Translate(dir * distance * (1 / timeOfArrival) * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            print(Vector3.SignedAngle(transform.position, player.transform.position, Vector3.up));
+        }
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public GameObject player;
+    public float distance;
+    //[Range(-1, 1)]
+   // public float ang;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float ang = Random.Range(-1f, 1f);
+            GameObject clone = Instantiate(enemy, player.transform.position + (new Vector3 (Mathf.Sin(ang), 0, Mathf.Cos(ang)) * distance), Quaternion.identity);
+            clone.GetComponent<Enemy>().player = player;
+        }   
+    }
+}
