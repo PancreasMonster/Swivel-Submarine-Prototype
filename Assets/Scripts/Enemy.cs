8198aa1ad5a15dc6c3086436ad1c8aa4e136c99f
Patnjop <36369847+Patnjop@users.noreply.github.com>
2019-03-27 14:49:14 +0000

    Ryans classwork

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,12 +6,14 @@ public class Enemy : MonoBehaviour
 {
     public GameObject player;
     public float timeOfArrival;
-    float distance;
-
+    float distance, d;
+    bool startingToFire;
+    EnemySpawner ES;
 
     // Start is called before the first frame update
     void Start()
     {
+        ES = GameObject.Find("EnemySpawner").GetComponent<EnemySpawner>();
         distance = Vector3.Distance(transform.position, player.transform.position);
         Vector3 dir = player.transform.position - (transform.position + (-Vector3.forward * 3));
         dir.Normalize();
@@ -21,9 +23,23 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 dir = player.transform.position - (transform.position + (-Vector3.forward * 3));
-        dir.Normalize();
-        transform.position += (dir * distance * (1 / timeOfArrival) * Time.deltaTime);
+        if (!startingToFire)
+        {
+            Vector3 dir = player.transform.position - (transform.position + (-Vector3.forward * 3));
+            dir.Normalize();
+            transform.position += (dir * distance * (1 / timeOfArrival) * Time.deltaTime);
+        }
+
+        if (Vector3.Distance(player.transform.position, transform.position) < (10 / ES.enemyCount) && !startingToFire)
+        {
+            startingToFire = true;
+            distance = Vector3.Distance(transform.position, player.transform.position);
+        }
+
+        if (startingToFire)
+        {
+           
+        }
 
         if (Input.GetKeyDown(KeyCode.E))
         {
@@ -35,6 +51,7 @@ public class Enemy : MonoBehaviour
     {
         if (col.transform.tag == "CannonBall")
         {
+            ES.enemyCount--;
             Destroy(col.transform.gameObject);
             Destroy(this.gameObject);
         }
--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -8,21 +8,32 @@ public class EnemySpawner : MonoBehaviour
     public GameObject player;
     public float distance, enemyRate;
     bool wait;
+    public float enemyCount;
     //[Range(-1, 1)]
    // public float ang;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
+        for(int i = 1; i < 4; i++)
+        {
+            for (int y = 0; y < 12; y++)
+            {
+                GameObject clone = Instantiate(GameObject.CreatePrimitive(PrimitiveType.Sphere),
+                    new Vector3(Mathf.Sin(-0.701f + (y * (2 * .701f / 6))) * 4 * i, 0, Mathf.Cos(-0.701f + (y * (2 * 0.701f / 6))) * 4 * i) + player.transform.position,
+                    Quaternion.identity);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-      if (!wait)
+      if (!wait && enemyCount < 3)
         {
             wait = true;
+            enemyCount++;
             float ang = Random.Range(-1f, 1f);
             GameObject clone = Instantiate(enemy, player.transform.position + (new Vector3 (Mathf.Sin(ang), 0, Mathf.Cos(ang)) * distance), Quaternion.identity);
             clone.GetComponent<Enemy>().player = player;
