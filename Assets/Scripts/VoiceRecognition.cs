c64eb68348e88a51997a84e28b3d546d2f9b70bd
PancreasMonster <ryanflood444@gmail.com>
2019-03-30 14:45:39 +0000

    Slowed Down Turnspeed, Made it Db's for Firing and Fixed Cannonball Firing from weird angle bug (I think)

--- Assets/Scripts/VoiceRecognition.cs
+++ Assets/Scripts/VoiceRecognition.cs
@@ -22,7 +22,7 @@ public class VoiceRecognition : MonoBehaviour
     private void Start()
     {
         //rb = initialBall.GetComponent<Rigidbody>();
-        actions.Add("fire", Fire);
+        /*actions.Add("fire", Fire);
         actions.Add("reload", ReloadTest);
         actions.Add("restart", Restart);
         actions.Add("pause", Pause);
@@ -30,17 +30,17 @@ public class VoiceRecognition : MonoBehaviour
 
         keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
         keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
-        keywordRecognizer.Start();
+        keywordRecognizer.Start();*/
 
         originalpos = this.transform.position;
         startPos = this.transform.position;
     }
 
-    private void RecognizedSpeech (PhraseRecognizedEventArgs speech)
+    /*private void RecognizedSpeech (PhraseRecognizedEventArgs speech)
     {
         Debug.Log(speech.text);
         actions[speech.text].Invoke();
-    }
+    }*/
 
     public void Fire ()
     {
@@ -117,6 +117,13 @@ public class VoiceRecognition : MonoBehaviour
         {
             this.transform.position = startPos;
         }
+        MicLoudness = LevelMax();
+        testSound = MicLoudness;
+
+        if(testSound > .5f && !reloadable)
+        {
+            Fire();
+        }
     }
 
     IEnumerator FireCall()
@@ -134,5 +141,83 @@ public class VoiceRecognition : MonoBehaviour
             reloadable = true;
         }
     }
+
+    public float testSound;
+    public static float MicLoudness;
+    private string _device;
+    private AudioClip _clipRecord;
+    private int _sampleWindow = 128;
+    private bool _isInitialized;
+
+    void InitMic()
+    {
+        if (_device == null)
+        {
+            _device = Microphone.devices[0];
+            _clipRecord = Microphone.Start(_device, true, 999, 44100);
+            Debug.Log(_clipRecord);
+        }
+    }
+
+    void StopMicrophone()
+    {
+        Microphone.End(_device);
+    }
+
+    float LevelMax()
+    {
+        float levelMax = 0;
+        float[] waveData = new float[_sampleWindow];
+        int micPosition = Microphone.GetPosition(_device) - (_sampleWindow + 1);
+        if (micPosition < 0)
+        {
+            return 0;
+        }
+        _clipRecord.GetData(waveData, micPosition);
+        for (int i = 0; i < _sampleWindow; ++i)
+        {
+            float wavePeak = waveData[i] * waveData[i];
+            if (levelMax < wavePeak)
+            {
+                levelMax = wavePeak;
+            }
+        }
+        return levelMax;
+    }
+
+    void OnEnable()
+    {
+        InitMic();
+        _isInitialized = true;
+    }
+
+    void OnDisable()
+    {
+        StopMicrophone();
+    }
+
+    void OnDestory()
+    {
+        StopMicrophone();
+    }
+
+    void OnApplicationFocus(bool focus)
+    {
+        if (focus)
+        {
+            if (!_isInitialized)
+            {
+                InitMic();
+                _isInitialized = true;
+            }
+        }
+
+        if (!focus)
+        {
+            StopMicrophone();
+            _isInitialized = false;
+        }
+    }
+
 }
-#endif 
+#endif
